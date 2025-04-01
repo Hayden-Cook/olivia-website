8acf8dd120e27d2c46820ca88a433b3f8ebc5b43
Hayden  Cook <haydencook@Haydens-MBP.attlocal.net>
2025-03-31 22:29:26 -0500

    adding config files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      './app/**/*.{js,ts,jsx,tsx}',   // Include files in the app directory
+      './pages/**/*.{js,ts,jsx,tsx}', // Include files in the pages directory
+      './components/**/*.{js,ts,jsx,tsx}', // Include files in the components directory
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+};
